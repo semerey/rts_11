45485b8f9bd5a8f22851baf014bb7310184fa4a8
semerey <fyzctvthtq@gmail.com>
2020-04-02 18:31:17 +0300

    new commit

--- rts_1_1.py
+++ rts_1_1.py
@@ -1,6 +1,15 @@
 import matplotlib.pyplot as plt
 import random
 import math
+import time
+
+def func_time(f):
+    def func(*args):
+        start_time = time.time()
+        a = f(*args)
+        print(f"({f.__name__})час:", (time.time() - start_time))
+        return a
+    return func
 
 n = 12
 omega = 2700
@@ -8,9 +17,10 @@ N = 64
 range_min = 0
 range_max = 1
 
-def signal(n, omega, N, min_value=0, max_value=1):
-    A = [min_value + (max_value - min_value) * random.random() for _ in range(n)]
-    phi = [min_value + (max_value - min_value) * random.random() for _ in range(n)]
+#@func_time
+def signal_func(n, omega, N, min_v=0, max_v=1):
+    A = [min_v + (max_v - min_v) * random.random() for _ in range(n)]
+    phi = [min_v + (max_v - min_v) * random.random() for _ in range(n)]
 
     def f(t):
         x = 0
@@ -19,18 +29,20 @@ def signal(n, omega, N, min_value=0, max_value=1):
         return x
     return f
 
-def get_m(x):
+@func_time
+def M(x):
     return sum(x)/len(x)
 
-def get_D(x, m=None):
+@func_time
+def D(x, m=None):
     if m is None:
-        m = get_m(x)
+        m = M(x)
     return sum([(i - m) ** 2 for i in x]) / (len(x) - 1)
 
-s_gen = signal(n, omega, N, range_min, range_max)
+s_gen = signal_func(n, omega, N, range_min, range_max)
 s = [s_gen(i) for i in range(N)]
-m = get_m(s)
-D = get_D(s, m)
+m = M(s)
+D = D(s, m)
 print("m:", m)
 print("D:", D)
 
